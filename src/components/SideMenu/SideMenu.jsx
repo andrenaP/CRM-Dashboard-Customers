9657d40c263b2736cd13676d03f400b0d0e3ba92
andrenaP <a0993100106@gmail.com>
2024-01-24 14:53:14 +0200

    add SideMenuLink

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,11 +1,20 @@
 import {Logo} from "components/SideMenu/Logo"
 import './SideMenu.styles.css';
 import {Profile} from "./Profile"
+import {SideMenuLink} from "./SideMenuLink"
+import {Dashboard,Product,Customers,Income,Promote,Help,LogoSvg} from "img/icons"
 export const SideMenu = () => {
     return (
       <div className="SideMenu">
         <Logo/>
-      SideMenu
+      <ul className="SideMenu__Ul">
+        <SideMenuLink  Name="Dashboard" link="/12"> <Dashboard className="SideMenu__Ul__Li__Link__Svg"/> </SideMenuLink>
+        <SideMenuLink Name="Product" link="/23">  <Product className="SideMenu__Ul__Li__Link__Svg" /> </SideMenuLink>
+        <SideMenuLink Name="Customers" link="/23">  <Customers className="SideMenu__Ul__Li__Link__Svg" /> </SideMenuLink>
+        <SideMenuLink Name="Income" link="/23">  <Income className="SideMenu__Ul__Li__Link__Svg" /> </SideMenuLink>
+        <SideMenuLink Name="Promote" link="/23">  <Promote className="SideMenu__Ul__Li__Link__Svg" /> </SideMenuLink>
+        <SideMenuLink Name="Help" link="/23">  <Help className="SideMenu__Ul__Li__Link__Svg" /> </SideMenuLink>
+      </ul>
       <Profile/>
       </div>
     );
--- src/components/SideMenu/SideMenuLink.jsx
+++ src/components/SideMenu/SideMenuLink.jsx
@@ -1,9 +1,18 @@
 import './SideMenu.styles.css';
-export const SideMenuLink = (Name,Icon,link="/") => {
-    return (
-      <div>
-        SideMenuLink
-      </div>
+import {Chevron_right} from "img/icons"
+export const SideMenuLink = (props) => {
+  const Icon=props.Icon
+  console.log(Icon)
+    return (<li className='SideMenu__Ul__Li'>
+      <a href={props.link} className='SideMenu__Ul__Li__Link'>
+        <div className='SideMenu__Ul__Li__Link__Text'>
+        {props.children}
+        <span className='SideMenu__Ul__Li__Link__Text__Name'>{props.Name}</span>
+        </div>
+        <Chevron_right className="SideMenu__Ul__Li__Link__Chevron_right"/>
+      </a>
+      </li>
+
     );
   };
   
